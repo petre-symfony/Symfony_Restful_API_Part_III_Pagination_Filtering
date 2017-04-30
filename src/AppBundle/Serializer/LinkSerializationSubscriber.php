fcf2b6e17b91b470feb690fc016d34ea8e4fd2d4
petrero <petrero@gmail.com>
2017-04-30 10:49:19 +0300

    Copy code from branch 7

---
+++ src/AppBundle/Serializer/LinkSerializationSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+namespace AppBundle\Serializer;
+
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use JMS\Serializer\JsonSerializationVisitor;
+use Symfony\Component\Routing\RouterInterface;
+use AppBundle\Entity\Programmer;
+
+class LinkSerializationSubscriber implements EventSubscriberInterface{
+  private $router;
+  
+  public function __construct(RouterInterface $router) {
+    $this->router = $router;
+  }
+
+    public function onPostSerialize(ObjectEvent $event){
+    /** @var JsonSerializationVisitor $visitor */
+    $visitor = $event->getVisitor(); 
+    /** @var Programmer $Programmer */
+    $programmer = $event->getObject();
+    $visitor->addData('uri', $this->router->generate(
+      'api_programmers_show',
+      array('nickname' => $programmer->getNickname())
+    ));
+  }
+
+
+  public static function getSubscribedEvents() {
+    return array(
+      array(
+        'event'  => 'serializer.post_serialize',
+        'method' => 'onPostSerialize',
+        'format' => 'json',
+        'class'  => 'AppBundle\Entity\Programmer'
+      )    
+    );
+  }
+}
