10813502e735b8e58f6c8ca36dd53982d107dda6
petrero <petrero@gmail.com>
2017-04-30 10:16:44 +0300

    The Serialization Visitor
    Back in the subscriber, create a new variable called  and set it to {dollar_sign}event->getVisitor(). The visitor is kind of in charge of the serialization process. And since we know we're serializing to JSON, this will be an instance of JsonSerializationVisitor. Write an inline doc for that and add a use statement up top. That will give us autocompletion.
    
    Oh, hey, look at this - that class has a method on it called addData(). We can use it to add whatever cool custom fields we want. Add that new uri field, but just set it to the classic FOO value for now.
    
    Registering the Subscriber
    
    The last thing we need to do - which you can probably guess - is register this as a service. In services.yml, add the service - how about link_serialization_subscriber. Add the class and skip arguments - we don't have any yet. But we do need a tag so that the JMS Serializer knows about our class. Set the tag name to jms_serializer.event_subscriber.
    
    Ok, try the test! Copy the method name, head to the terminal and run:
    
    bin/phpunit -c app --filter testGETProgrammer src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
    and then paste in the name. This method name matches a few tests, so we'll see more than just our one test run. Yes, it fails... but in a good way!
    
    FOO does not match /api/programmers/UnitTester.
    Above, we do have the new, custom uri field.

--- src/AppBundle/Serializer/LinkSerializationSubscriber.php
+++ src/AppBundle/Serializer/LinkSerializationSubscriber.php
@@ -3,14 +3,17 @@ namespace AppBundle\Serializer;
 
 use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
 use JMS\Serializer\EventDispatcher\ObjectEvent;
+use JMS\Serializer\JsonSerializationVisitor;
 
 class LinkSerializationSubscriber implements EventSubscriberInterface{
   public function onPostSerialize(ObjectEvent $event){
-    
+    /** @var JsonSerializationVisitor $visitor */
+    $visitor = $event->getVisitor(); 
+    $visitor->addData('uri', 'FOO');
   }
 
 
-  public function getSubscribedEvents() {
+  public static function getSubscribedEvents() {
     return array(
       array(
         'event'  => 'serializer.post_serialize',
