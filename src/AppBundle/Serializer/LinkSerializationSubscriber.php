c52bd31a9d88b8ffe978e8d6604d0bd7a883d3ca
petrero <petrero@gmail.com>
2017-04-30 13:22:11 +0300

    Reading the Annotation
    Ok... so how do we read annotations? Great question, I have no idea. Ah, it's easy, thanks to the Doctrine annotations library that comes standard with Symfony. In fact, we already have a service available called @annotation_reader.
    
    Inside LinkSerializationSubscriber, inject that as the second argument. It's an instance of the Reader interface from Doctrine\Common\Annotations. Call it {dollar_sign}annotationsReader.
    
    I'll hit option+enter and select initialize fields to get that set on property.
    
    And before I forget, in services.yml, inject that by adding @annotation_reader as the second argument.
    
    Super easy.
    
    Too easy, back to work! Delete all of this junk in onPostSerialize() and start with {dollar_sign}object = {dollar_sign}event->getObject(). To read the annotations off of that object, add {dollar_sign}annotations = {dollar_sign}this->annotationReader->getClassAnnotations(). Pass that a new \ReflectionObject() for {dollar_sign}object.
    
    That's it!
    
    Now, the class could have a lot of annotations above it, but we're only interested in the @Link annotation. We'll add an if statement to look for that in a second. But first, create {dollar_sign}links = array(): that'll be our holder for any links we find.
    
    Now, foreach ( as ). Immediately, see if this is something we care about with if ({dollar_sign}annotation instanceof Link). At this point, the annotation options are populated on the public properties of the Link object. To get the URI, we can just say {dollar_sign}this->router->generate() and pass it {dollar_sign}annotation->route and {dollar_sign}annotation->params.
    
    How sweet is that? Well, we're not done yet: the params contain an expression string... which we're not parsing yet. We'll get back to that in a second.
    
    Finish this off with {dollar_sign}links[{dollar_sign}annotation->name] = {dollar_sign}uri;. At the bottom, finish with the familiar {dollar_sign}visitor->addData() with _links set to {dollar_sign}links;. Other than evaluating the expression, that's all the code you need.
    
    Check this out by going to /api/programmers in the browser. Look at that! The embedded programmer entities actually have a link called self. It worked!
    
    Of course, the link is totally wrong because we're not evaluating the expression yet. But, we're really close.

--- src/AppBundle/Serializer/LinkSerializationSubscriber.php
+++ src/AppBundle/Serializer/LinkSerializationSubscriber.php
@@ -4,25 +4,41 @@ namespace AppBundle\Serializer;
 use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
 use JMS\Serializer\EventDispatcher\ObjectEvent;
 use JMS\Serializer\JsonSerializationVisitor;
+use Doctrine\Common\Annotations\Reader;
 use Symfony\Component\Routing\RouterInterface;
 use AppBundle\Entity\Programmer;
+use AppBundle\Annotation\Link;
 
 class LinkSerializationSubscriber implements EventSubscriberInterface{
   private $router;
+  private $annotationReader;
   
-  public function __construct(RouterInterface $router) {
+  public function __construct(RouterInterface $router, Reader $annotationReader) {
     $this->router = $router;
+    $this->annotationReader = $annotationReader;
   }
 
     public function onPostSerialize(ObjectEvent $event){
     /** @var JsonSerializationVisitor $visitor */
     $visitor = $event->getVisitor(); 
-    /** @var Programmer $Programmer */
-    $programmer = $event->getObject();
-    $visitor->addData('uri', $this->router->generate(
-      'api_programmers_show',
-      array('nickname' => $programmer->getNickname())
-    ));
+    
+    $object = $event->getObject();
+    $annotations = $this->annotationReader
+      ->getClassAnnotations(new \ReflectionObject($object));
+    $links = array();
+    
+    foreach ($annotations as $annotation){
+      if ($annotation instanceof Link){
+        $uri = $this->router->generate(
+          $annotation->route, 
+          $annotation->params
+        );
+        
+        $links[$annotation->name] = $uri;
+      }
+    }
+    
+    $visitor->addData('_links', $links);
   }
 
 
